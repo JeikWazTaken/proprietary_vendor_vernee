e1b9245771a10853317df5a6e8627bb43f1b7d11
TheMarionette <the.freaking.marionette@gmail.com>
2017-05-06 00:05:40 +0200

    add README

---
+++ README.mk
@@ -0,0 +1,64 @@
+Elephone P9000 - LineageOS 13.0/AOSP
+==============
+
+This branch is vendor for building of the LineageOS 13.0 and Android Marshmallow 6.0 AOSP ROMs.
+
+---
+
+# About Device
+
+![Vernee Mars](https://d2giyh01gjb6fi.cloudfront.net/default/0001/37/thumb_36751_default_big.jpeg "Vernee Mars in silver")
+
+Released on September 2016, the Vernee Mars (codenamed _"k11tc_a"_) is a eye-candy phone released by Vernee. The Mars now supports LineageOS 13.0!
+
+### Specifications
+
+Component Type | Details
+-------:|:-------------------------
+CPU     | 2.0GHz Octa-Core MT6755 (Helio P10)
+GPU     | Mali-T860
+Memory  | 4GB RAM
+Shipped Android Version | 6.0
+Storage | 32GB
+Battery | 3000 mAh
+Display | 5.5" 1920 x 1080 px DPI 480
+Rear Camera | 13MP (Sony IMX258 Exmor RS), Dual LED Flash
+Front Camera | 5MP (OV)
+
+---
+
+# Build Information
+
+### Working:
+ * Hardware acceleration
+ * Wi-Fi
+ * Fast charging
+ * Offline charging
+ * Vibration
+ * Audio
+ * Auto brightness
+ * Doze
+ * RAM and ROM
+ * Rotation
+ * All sensors
+ * Flashlight (statusbar and camera)
+ * Bluetooth
+ * Camera rear/front (video and photo)
+ * Camera HDR
+ * Micro SD support
+ * RIL for SMS/calls/internet/LTE on 1 and 2 SIM
+ * MTP and Mass Storage modes
+ * Audio over Bluetooth (music, calls .etc)
+ * Fingerprint scanner
+ * FM radio
+ * Wi-Fi tethering
+
+### In progress:
+ * GPS
+ * getting rid of the pink filter on the front camera
+
+### Thanks to:
+ * LineageOS team
+ * Alberto96
+
+
